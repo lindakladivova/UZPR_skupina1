aecdfde6be683bb5ecf0275b1c157b8d62773586
lukaskett <k.lukas15@seznam.cz>
2020-01-31 15:35:13 +0100

    Úprava skriptů pro potřeby správných souř. sys.

--- data/dlazdice/tippecanoe.sh
+++ data/dlazdice/tippecanoe.sh
@@ -3,9 +3,9 @@
 mkdir -p scratch
 
 # Run tippecanoe - build vector tiles
-tippecanoe -o ./scratch/tiles_5514.mbtiles -zg -pk -pC -pS -pt -f ../geojson_obec_boskovice_5514/608483_HRANICE_PARCEL_L.geojson
+tippecanoe -o ./scratch/tiles_4326.mbtiles -zg -pk -pC -pS -pt -f ../geojson_obec_boskovice_4326/608483_HRANICE_PARCEL_L.geojson
 
 # Unpackage - mb util
 rm -r -f tiles
 #creates folder tiles automatically
-mb-util ./scratch/tiles_5514.mbtiles ./tiles --image_format=pbf
+mb-util ./scratch/tiles_4326.mbtiles ./tiles --image_format=pbf
