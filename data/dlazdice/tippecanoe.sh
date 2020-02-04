c3e8114dae92f1b83cce5c3b71a44ba468e0dd73
lukaskett <k.lukas15@seznam.cz>
2020-02-04 22:39:52 +0100

    Vyladění skriptu pro generování
    Generuje a rozbalí dlaždice pro celou obec Boskovice pro vrstvy budov_p a hranice_parcel_l
    
    Původně jsem chtěl obě databáze spojit pomocí tile-join utility, ale nejde to a asi to ani nedává smysl

--- data/dlazdice/tippecanoe.sh
+++ data/dlazdice/tippecanoe.sh
@@ -3,9 +3,55 @@
 mkdir -p scratch
 
 # Run tippecanoe - build vector tiles
-tippecanoe -o ./scratch/tiles.mbtiles -zg -pk -pC -pS -pt -f ../geojson_obec_boskovice_4326/608483_HRANICE_PARCEL_L.geojson
+#1) PHranice parcel 
+tippecanoe \
+-n Boskovice \
+-N 'Sample experimental tileset' \
+-l hranice_parcel_l  \
+-o ./scratch/hranice_parcel_l.mbtiles \
+-zg \
+-pk \
+-pC \
+-pS \
+-pt \
+-f \
+--coalesce-densest-as-needed \
+--extend-zooms-if-still-dropping  \
+../geojson_obec_boskovice_4326/600822_HRANICE_PARCEL_L.geojson \
+../geojson_obec_boskovice_4326/608327_HRANICE_PARCEL_L.geojson \
+../geojson_obec_boskovice_4326/608475_HRANICE_PARCEL_L.geojson \
+../geojson_obec_boskovice_4326/608483_HRANICE_PARCEL_L.geojson \
+../geojson_obec_boskovice_4326/785598_HRANICE_PARCEL_L.geojson
 
-# Unpackage - mb util
+#2) Budovy
+tippecanoe \
+-n Boskovice \
+-N 'Sample experimental tileset' \
+-l budovy_p  \
+-o ./scratch/budovy_p.mbtiles \
+-zg \
+-pk \
+-pC \
+-pS \
+-pt \
+-f \
+--coalesce-densest-as-needed \
+--extend-zooms-if-still-dropping  \
+../geojson_obec_boskovice_4326/600822_BUDOVY_P.geojson \
+../geojson_obec_boskovice_4326/608327_BUDOVY_P.geojson \
+../geojson_obec_boskovice_4326/608475_BUDOVY_P.geojson \
+../geojson_obec_boskovice_4326/608483_BUDOVY_P.geojson \
+../geojson_obec_boskovice_4326/785598_BUDOVY_P.geojson
+
+# Merge MVTiles
+#tile-join -o boskovice.mbtiles \
+#-pk \
+#-f \
+#./scratch/budovy_p.mbtiles \
+#./scratch/hranice_parcel_l.mbtiles
+
+# Unpackage tiles - mb util
 rm -r -f tiles
 #creates folder tiles automatically
-mb-util ./scratch/tiles.mbtiles ./tiles --image_format=pbf
+mb-util ./scratch/hranice_parcel_l.mbtiles ./tiles --image_format=pbf
+mb-util ./scratch/budovy_p.mbtiles ./tiles_budovy --image_format=pbf
